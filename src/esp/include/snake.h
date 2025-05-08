e8c012d689a6d2e6afdcd1fe6112f1c110ce3e7e
lemz1 <ismael.amjad07@gmail.com>
2025-05-08 21:34:59 +0200

    refactor esp code

--- src/esp/include/snake.h
+++ src/esp/include/snake.h
@@ -5,25 +5,22 @@
 #include <esp_now.h>
 #include <stdint.h>
 
-enum SteuerStationButton 
-{
-  GREEN_BUTTON  = 0,
+enum SteuerStationButton {
+  GREEN_BUTTON = 0,
   YELLOW_BUTTON = 1,
-  RED_BUTTON    = 2,
-  BLUE_BUTTON   = 3,
+  RED_BUTTON = 2,
+  BLUE_BUTTON = 3,
 
   // currently unsued, might be relevant in the future
-  WHITE_BUTTON  = 4,
+  WHITE_BUTTON = 4,
 };
 
-struct SteuerStationMessage 
-{
+struct SteuerStationMessage {
   SteuerStationButton button;
 };
 
 #define checkEspRes(res, msg)                                                  \
-  if (res != ESP_OK)                                                           \
-  {                                                                            \
+  if (res != ESP_OK) {                                                         \
     Serial.print("(File: ");                                                   \
     Serial.print(__FILE__);                                                    \
     Serial.print(", Line: ");                                                  \
